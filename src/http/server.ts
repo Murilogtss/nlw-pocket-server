54e3f3c02d63a2c2d28e15e15a966df27df4b46d
murilogtss <murilogtss123@gmail.com>
2024-09-11 00:02:28 -0300

    feat: add ZodTypeProvider to goals route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,21 +1,36 @@
 import fastify from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
 import { createGoal } from '../functions/create-goal'
 import z from 'zod'
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
-app.post('/goals', async (req, res) => {
-  const createGoalSchema = z.object({
-    title: z.string(),
-    desiredWeeklyFrequency: z.number().int().min(1).max(7),
-  })
-  const body = createGoalSchema.parse(req.body)
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
 
-  await createGoal({
-    title: body.title,
-    desiredWeeklyFrequency: body.desiredWeeklyFrequency,
-  })
-})
+app.post(
+  '/goals',
+  {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        desiredWeeklyFrequency: z.number().int().min(1).max(7),
+      }),
+    },
+  },
+  async req => {
+    const { title, desiredWeeklyFrequency } = req.body
+
+    await createGoal({
+      title,
+      desiredWeeklyFrequency,
+    })
+  }
+)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP Server running on http://localhost:3333')
